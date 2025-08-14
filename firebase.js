6efc4719f268aff460f7927dd60cc353c802e786
Utkarsh Verma <uttkarsh04@gmail.com>
2025-08-14 23:09:27 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,15 +4,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHgMDHxvsdXhHSiBGTPNp4PZ6bOIPZLrY",
-  authDomain: "clinic-management-system-20392.firebaseapp.com",
-  projectId: "clinic-management-system-20392",
-  storageBucket: "clinic-management-system-20392.appspot.com",
-  messagingSenderId: "973761551856",
-  appId: "1:973761551856:web:f7e24ccece0deb5c201135",
-  measurementId: "G-XS9J079WX0"
+  apiKey: "AIzaSyBS3ACmy5c-Pa2_kWSNnfDF_BRZo8qqcyw",
+  authDomain: "clinic-management-system-27203.firebaseapp.com",
+  projectId: "clinic-management-system-27203",
+  storageBucket: "clinic-management-system-27203.appspot.com",
+  messagingSenderId: "444012084756",
+  appId: "1:444012084756:web:cf52cbc791481df1c07101",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
